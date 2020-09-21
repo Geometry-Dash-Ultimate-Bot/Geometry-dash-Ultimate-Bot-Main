b73a83bc4d35f45dd3e54190e588876bb4c76465
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-09-20 22:00:07 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const { prefix, token, status } = require('./config.json');
 const bot = require('./src/bot.json');
 const emote = require('./src/emotes');
+const string = require('./strings/english');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -19,7 +20,7 @@ client.once('ready', () => {
     client.user.setActivity(`${status}`, {
   type: "PLAYING"
 });
-    client.channels.cache.get(bot.log).send(`${emote.info} Event Fired: Bot Login as: \`${client.user.tag}\``);
+    client.channels.cache.get(bot.log).send(`${emote.info} ${string.turn_on} \`${client.user.tag}\``);
     console.log(`${client.user.tag} is ready!`);
 });
 
@@ -35,8 +36,10 @@ client.on('message', message => {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.channel.send(` ${emote.x} unable to execute command!`);
-	}
+		message.channel.send(`${emote.x} ${string.error}`);
+                message.channel.send(`${emote.check} i have sent a log to the developer!`);
+                client.channels.cache.get(bot.log).send(`${emote.x} ${string.error} while executing a command!`);
+	};
 });
 
 client.login(token);
